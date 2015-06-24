f407803236152a3efb11b681d808c35d029e3807
Yanghg <yanghong2014@u.northwestern.edu>
2015-06-24 21:28:45 +0800

    add new

---
+++ ContainsDuplicate2.java
@@ -0,0 +1,12 @@
+//Integer to int ==> Obj.intValue();
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < nums.length; i++){
+            if (!map.containsKey(nums[i]) || Math.abs(i - map.get(nums[i])) > k)
+                map.put(nums[i],i);
+            else return true;
+        }
+        return false;
+    }
+}
